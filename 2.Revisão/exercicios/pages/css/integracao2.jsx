a73ca9c92bb362591fa62ac5043fc755c80b94bd
cledsonfs-cmd <cledsonfs@gmail.com>
2023-10-07 19:28:43 -0300

    ajustes

--- 2.Revisão/exercicios/pages/css/integracao2.jsx
+++ 2.Revisão/exercicios/pages/css/integracao2.jsx
@@ -1,9 +1,9 @@
 import styles from "./integracao2.module.css"
 
 export default function integracao2() {
-    <div className="integracao1">
+    return (<div className={styles.integracao1}>
         <div className={styles.vermelha}>Texto #01</div>
         <div className={styles.azul}>Texto #2</div>
         <div className={styles.branca}>Texto #3</div>
-    </div>
+    </div>)
 }
