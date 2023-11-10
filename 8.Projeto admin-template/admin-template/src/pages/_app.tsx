88fc8da37ac0530cc4e709d7369706c4e9824b85
Cledson Francisco Silva <70558441+cledsonfs-cmd@users.noreply.github.com>
2023-11-10 12:23:12 -0300

    10-11-203-1

---
+++ 8.Projeto admin-template/admin-template/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProvider } from '../data/context/AppContext'
+import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  )
+}
+
+export default MyApp
